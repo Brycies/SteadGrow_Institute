11bf4edac98b004fef273aaad837c2d58c130933
Brycies <brysonodhiambo29@gmail.com>
2024-03-24 17:55:16 +0300

    commit changes

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -2,49 +2,44 @@
 $errorMSG = "";
 
 if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
+    $errorMSG .= "Name is required. ";
 } else {
     $name = $_POST["name"];
 }
 
 if (empty($_POST["email"])) {
-    $errorMSG = "Email is required ";
+    $errorMSG .= "Email is required. ";
 } else {
     $email = $_POST["email"];
 }
 
 if (empty($_POST["message"])) {
-    $errorMSG = "Message is required ";
+    $errorMSG .= "Message is required. ";
 } else {
     $message = $_POST["message"];
 }
 
-if (empty($_POST["terms"])) {
-    $errorMSG = "Terms is required ";
+if (!isset($_POST["terms"])) {
+    $errorMSG .= "Terms checkbox must be checked. ";
 } else {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Corso landing page";
+$EmailTo = "brysonodhiambo29@gmail.com";
+$Subject = "New message from SteadGrow Training Institute";
 
 // prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-$Body .= "Terms: ";
-$Body .= $terms;
-$Body .= "\n";
+$Body = "Name: $name\n";
+$Body .= "Email: $email\n";
+$Body .= "Message: $message\n";
+$Body .= "Terms: $terms\n";
+
+// additional headers
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
 
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+$success = mail($EmailTo, $Subject, $Body, $headers);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
--- php/newsletterform-process.php
+++ php/newsletterform-process.php
@@ -1,41 +1,38 @@
 <?php
 $errorMSG = "";
 
-if (empty($_POST["email"])) {
-    $errorMSG = "Email is required ";
+if (empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    $errorMSG .= "Valid email is required. ";
 } else {
     $email = $_POST["email"];
 }
 
 if (empty($_POST["terms"])) {
-    $errorMSG = "Terms is required ";
+    $errorMSG .= "Terms must be accepted. ";
 } else {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New newsletter subscription from Corso landing page";
+if (empty($errorMSG)) {
+    $EmailTo = "brysonodhiambo29@gmail.com";
+    $Subject = "New newsletter subscription from Steadgrow Training Institute";
 
-// prepare email body text
-$Body = "";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Terms: ";
-$Body .= $terms;
-$Body .= "\n";
+    // prepare email body text
+    $Body = "Email: " . $email . "\n";
+    $Body .= "Terms: " . $terms . "\n";
 
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+    // send email
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    $success = mail($EmailTo, $Subject, $Body, $headers);
 
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
+    if ($success){
+        echo "success";
     } else {
-        echo $errorMSG;
+        echo "Something went wrong :(";
     }
+} else {
+    echo $errorMSG;
 }
 ?>
